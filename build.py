1e7dfe8f9c38ee535d37412bb9558599d509516b
Anthony Sifontes <anthony.sifontes@gmail.com>
2019-02-10 22:52:20 -0800

    fixed directory reference for site files from directory of build script to docs directory

--- build.py
+++ build.py
@@ -3,14 +3,15 @@ print("website fragments... assemble!!!")
 top = open('templates/top.html').read()
 bottom = open('templates/bottom.html').read()
 print("top and bottom are in position")
+
 # Read index
 print("reading index...")
 index = open('content/index.html').read()
 
 # Assemble index
-open('index.html', 'a+').write(top)
-open('index.html', 'a+').write(index)
-open('index.html', 'a+').write(bottom)
+open('docs/index.html', 'a+').write(top)
+open('docs/index.html', 'a+').write(index)
+open('docs/index.html', 'a+').write(bottom)
 
 
 # Read bio
@@ -18,9 +19,9 @@ print("reading bio...")
 bio = open('content/bio.html').read()
 
 # Assemble bio
-open('bio.html', 'a+').write(top)
-open('bio.html', 'a+').write(bio)
-open('bio.html', 'a+').write(bottom)
+open('docs/bio.html', 'a+').write(top)
+open('docs/bio.html', 'a+').write(bio)
+open('docs/bio.html', 'a+').write(bottom)
 
 
 # Read blog
@@ -29,9 +30,9 @@ blog = open('content/blog.html').read()
 
 # Assemble blog
 
-open('blog.html', 'a+').write(top)
-open('blog.html', 'a+').write(blog)
-open('blog.html', 'a+').write(bottom)
+open('docs/blog.html', 'a+').write(top)
+open('docs/blog.html', 'a+').write(blog)
+open('docs/blog.html', 'a+').write(bottom)
 
 
 # Read projects
@@ -39,9 +40,8 @@ print("reading projects...")
 projects = open('content/projects.html').read()
 
 # Assemble projects
-
-open('projects.html', 'a+').write(top)
-open('projects.html', 'a+').write(projects)
-open('projects.html', 'a+').write(bottom)
+open('docs/projects.html', 'a+').write(top)
+open('docs/projects.html', 'a+').write(projects)
+open('docs/projects.html', 'a+').write(bottom)
 
 print("fragments assembled successfully! :)")
