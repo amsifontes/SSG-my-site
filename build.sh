829f7e98408a42ba49b25fb76d45178d5c89d8b1
Anthony Sifontes <anthony.sifontes@gmail.com>
2019-02-10 22:30:57 -0800

    add build.sh bash build script

---
+++ build.sh
@@ -0,0 +1,12 @@
+# index
+cat templates/top.html content/index.html templates/bottom.html > index.html
+
+
+# Bio
+cat templates/top.html content/bio.html templates/bottom.html > bio.html
+
+# Blog
+cat templates/top.html content/blog.html templates/bottom.html > blog.html
+
+# Projects
+cat templates/top.html content/projects.html templates/bottom.html > projects.html
