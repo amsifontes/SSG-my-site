7854b2873bb866e99695d7a66ab3de185f6862be
Anthony Sifontes <anthony.sifontes@gmail.com>
2019-02-10 22:56:52 -0800

    fix bash build script to build html files in docs directory

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 # index
-cat templates/top.html content/index.html templates/bottom.html > index.html
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
 
 
 # Bio
-cat templates/top.html content/bio.html templates/bottom.html > bio.html
+cat templates/top.html content/bio.html templates/bottom.html > docs/bio.html
 
 # Blog
-cat templates/top.html content/blog.html templates/bottom.html > blog.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
 
 # Projects
-cat templates/top.html content/projects.html templates/bottom.html > projects.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
