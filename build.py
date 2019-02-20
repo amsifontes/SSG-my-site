fbd1e912194c77feb047a2d6701d2ff532b5fdf5
Anthony Sifontes <anthony.sifontes@gmail.com>
2019-02-19 23:14:02 -0800

    stage 5 complete, multiple dynamic templating insert points supported

--- build.py
+++ build.py
@@ -31,64 +31,18 @@ def main():
         ]
 
     print("website fragments... assemble!!!")
-    # ingest top and bottom
-    # top = open('templates/top.html').read()
-    # bottom = open('templates/bottom.html').read()
-    # print("top and bottom are in position")
 
-    #ingest base template
+    # ingest base template
     template = ingest_base('templates/base.html')
 
+    # ingest dictionary for each page and assemble using metadata,
+    # inserting/replacing title and content
     for page in pages:
         content = open(page['input']).read()
-        # full_page = template.replace("{{ content }}", content)
         full_page = content_insert(template, content)
-        open(page['output'], 'w+').write(full_page)
+        titled_full_page = content_insert(full_page, page['title'], insert_marker="{{ title }}")
+        open(page['output'], 'w+').write(titled_full_page)
 
-        #
-        # open(page['output'], 'a+').write(top)
-        # open(page['output'], 'a+').write(content)
-        # open(page['output'], 'a+').write(bottom)
-
-    # # Read index
-    # print("reading index...")
-    # index = open('content/index.html').read()
-    #
-    # # Assemble index
-    # open('docs/index.html', 'a+').write(top)
-    # open('docs/index.html', 'a+').write(index)
-    # open('docs/index.html', 'a+').write(bottom)
-    #
-    #
-    # # Read bio
-    # print("reading bio...")
-    # bio = open('content/bio.html').read()
-    #
-    # # Assemble bio
-    # open('docs/bio.html', 'a+').write(top)
-    # open('docs/bio.html', 'a+').write(bio)
-    # open('docs/bio.html', 'a+').write(bottom)
-    #
-    #
-    # # Read blog
-    # print("reading blog...")
-    # blog = open('content/blog.html').read()
-    #
-    # # Assemble blog
-    #
-    # open('docs/blog.html', 'a+').write(top)
-    # open('docs/blog.html', 'a+').write(blog)
-    # open('docs/blog.html', 'a+').write(bottom)
-    #
-    #
-    # # Read projects
-    # print("reading projects...")
-    # projects = open('content/projects.html').read()
-    #
-    # # Assemble projects
-    # open('docs/projects.html', 'a+').write(top)
-    # open('docs/projects.html', 'a+').write(projects)
-    # open('docs/projects.html', 'a+').write(bottom)
 
     print("fragments assembled successfully! :)")
 
