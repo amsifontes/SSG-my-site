547cfba29b227ba699e95b4549f9ff4b30280832
Anthony Sifontes <anthony.sifontes@gmail.com>
2019-02-19 20:36:50 -0800

    phase 2 complete - refactored explicit assembly process into for loop, looping over  list of dictionaries

--- build.py
+++ build.py
@@ -31,45 +31,51 @@ def main():
     bottom = open('templates/bottom.html').read()
     print("top and bottom are in position")
 
-    # Read index
-    print("reading index...")
-    index = open('content/index.html').read()
+    for page in pages:
+        content = open(page['input']).read()
+        open(page['output'], 'a+').write(top)
+        open(page['output'], 'a+').write(content)
+        open(page['output'], 'a+').write(bottom)
 
-    # Assemble index
-    open('docs/index.html', 'a+').write(top)
-    open('docs/index.html', 'a+').write(index)
-    open('docs/index.html', 'a+').write(bottom)
-
-
-    # Read bio
-    print("reading bio...")
-    bio = open('content/bio.html').read()
-
-    # Assemble bio
-    open('docs/bio.html', 'a+').write(top)
-    open('docs/bio.html', 'a+').write(bio)
-    open('docs/bio.html', 'a+').write(bottom)
-
-
-    # Read blog
-    print("reading blog...")
-    blog = open('content/blog.html').read()
-
-    # Assemble blog
-
-    open('docs/blog.html', 'a+').write(top)
-    open('docs/blog.html', 'a+').write(blog)
-    open('docs/blog.html', 'a+').write(bottom)
-
-
-    # Read projects
-    print("reading projects...")
-    projects = open('content/projects.html').read()
-
-    # Assemble projects
-    open('docs/projects.html', 'a+').write(top)
-    open('docs/projects.html', 'a+').write(projects)
-    open('docs/projects.html', 'a+').write(bottom)
+    # # Read index
+    # print("reading index...")
+    # index = open('content/index.html').read()
+    #
+    # # Assemble index
+    # open('docs/index.html', 'a+').write(top)
+    # open('docs/index.html', 'a+').write(index)
+    # open('docs/index.html', 'a+').write(bottom)
+    #
+    #
+    # # Read bio
+    # print("reading bio...")
+    # bio = open('content/bio.html').read()
+    #
+    # # Assemble bio
+    # open('docs/bio.html', 'a+').write(top)
+    # open('docs/bio.html', 'a+').write(bio)
+    # open('docs/bio.html', 'a+').write(bottom)
+    #
+    #
+    # # Read blog
+    # print("reading blog...")
+    # blog = open('content/blog.html').read()
+    #
+    # # Assemble blog
+    #
+    # open('docs/blog.html', 'a+').write(top)
+    # open('docs/blog.html', 'a+').write(blog)
+    # open('docs/blog.html', 'a+').write(bottom)
+    #
+    #
+    # # Read projects
+    # print("reading projects...")
+    # projects = open('content/projects.html').read()
+    #
+    # # Assemble projects
+    # open('docs/projects.html', 'a+').write(top)
+    # open('docs/projects.html', 'a+').write(projects)
+    # open('docs/projects.html', 'a+').write(bottom)
 
     print("fragments assembled successfully! :)")
 
